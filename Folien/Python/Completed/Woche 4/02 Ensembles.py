66208896c2359543b0d7f73d6bc4fcca68775c98
Matthias Hölzl <tc@xantira.com>
2025-06-06 04:29:38 +0200

    Add Feature Engineering slides

--- Folien/Python/Completed/Woche 4/02 Ensembles.py
+++ Folien/Python/Completed/Woche 4/02 Ensembles.py
@@ -7,6 +7,10 @@
 # <div style="text-align:center;">Dr. Matthias Hölzl</div>
 # <br/>
 
+# %% [markdown]
+#
+# # Ensembles
+
 # %%
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,15 +18,15 @@ import seaborn as sns
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
+import evaluation_tools as et
 
+# %%
 sns.set_theme()
 
 # %%
 rng = np.random.default_rng(42)
-
 x = rng.uniform(size=(150, 1), low=0.0, high=10.0)
 x_train, x_test = x[:100], x[100:]
-
 x_plot = np.linspace(0, 10, 500).reshape(-1, 1)
 
 
@@ -33,177 +37,107 @@ def lin(x):
 
 # %%
 def fun(x):
-    return 2 * np.sin(x) + 0.1 * x ** 2 - 2
-
-
-# %%
-def randomize(fun, x, scale=0.5):
-    return fun(x) + rng.normal(size=x.shape, scale=scale)
-
-
-# %%
-def evaluate_non_random_regressor(reg_type, f_y, *args, **kwargs):
-    reg = reg_type(*args, **kwargs)
-
-    y_train = f_y(x_train).reshape(-1)
-    y_test = f_y(x_test).reshape(-1)
-
-    reg.fit(x_train, y_train)
-    y_pred = reg.predict(x_test)
-
-    x_plot = np.linspace(0, 10, 500).reshape(-1, 1)
-    fig, ax = plt.subplots(figsize=(20, 8))
-    sns.lineplot(x=x_plot[:, 0], y=reg.predict(x_plot), ax=ax)
-    sns.lineplot(x=x_plot[:, 0], y=f_y(x_plot[:, 0]), ax=ax)
-    sns.scatterplot(x=x_train[:, 0], y=y_train, ax=ax)
-    plt.show()
-
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print(
-        "\nNo randomness:      " f"MAE = {mae:.2f}, MSE = {mse:.2f}, RMSE = {rmse:.2f}"
-    )
-
-    return reg
-
-
-# %%
-def plot_graphs(f_y, reg, reg_rand, reg_chaos, y_train, y_rand_train, y_chaos_train):
-    x_plot = np.linspace(0, 10, 500).reshape(-1, 1)
-    fig, ax = plt.subplots(figsize=(20, 12))
-    sns.lineplot(x=x_plot[:, 0], y=reg.predict(x_plot), ax=ax)
-    sns.scatterplot(x=x_train[:, 0], y=y_train, ax=ax)
-
-    sns.lineplot(x=x_plot[:, 0], y=reg_rand.predict(x_plot), ax=ax)
-    sns.scatterplot(x=x_train[:, 0], y=y_rand_train, ax=ax)
-
-    sns.lineplot(x=x_plot[:, 0], y=reg_chaos.predict(x_plot), ax=ax)
-    sns.scatterplot(x=x_train[:, 0], y=y_chaos_train, ax=ax)
-
-    sns.lineplot(x=x_plot[:, 0], y=f_y(x_plot[:, 0]), ax=ax)
-    plt.show()
-
-
-# %%
-def print_evaluation(y_test, y_pred, y_rand_test, y_rand_pred, y_chaos_test, y_chaos_pred):
-    mae = mean_absolute_error(y_test, y_pred)
-    mae_rand = mean_absolute_error(y_rand_test, y_rand_pred)
-    mae_chaos = mean_absolute_error(y_chaos_test, y_chaos_pred)
-
-    mse = mean_squared_error(y_test, y_pred)
-    mse_rand = mean_squared_error(y_rand_test, y_rand_pred)
-    mse_chaos = mean_squared_error(y_chaos_test, y_chaos_pred)
-
-    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    rmse_rand = np.sqrt(mean_squared_error(y_rand_test, y_rand_pred))
-    rmse_chaos = np.sqrt(mean_squared_error(y_chaos_test, y_chaos_pred))
-
-    print(
-        "\nNo randomness:      " f"MAE = {mae:.2f}, MSE = {mse:.2f}, RMSE = {rmse:.2f}"
-    )
-    print(
-        "Some randomness:    "
-        f"MAE = {mae_rand:.2f}, MSE = {mse_rand:.2f}, RMSE = {rmse_rand:.2f}"
-    )
-    print(
-        "Lots of randomness: "
-        f"MAE = {mae_chaos:.2f}, MSE = {mse_chaos:.2f}, RMSE = {rmse_chaos:.2f}"
-    )
-
-
-# %%
-def evaluate_regressor(reg_type, f_y, *args, **kwargs):
-    reg = reg_type(*args, **kwargs)
-    reg_rand = reg_type(*args, **kwargs)
-    reg_chaos = reg_type(*args, **kwargs)
-
-    y_train = f_y(x_train).reshape(-1)
-    y_test = f_y(x_test).reshape(-1)
-    y_pred = reg.fit(x_train, y_train).predict(x_test)
-
-    y_rand_train = randomize(f_y, x_train).reshape(-1)
-    y_rand_test = randomize(f_y, x_test).reshape(-1)
-    y_rand_pred = reg_rand.fit(x_train, y_rand_train).predict(x_test)
-
-    y_chaos_train = randomize(f_y, x_train, 1.5).reshape(-1)
-    y_chaos_test = randomize(f_y, x_test, 1.5).reshape(-1)
-    y_chaos_pred = reg_chaos.fit(x_train, y_chaos_train).predict(x_test)
-
-    plot_graphs(f_y, reg, reg_rand, reg_chaos, y_train, y_rand_train, y_chaos_train)
-    print_evaluation(y_test, y_pred, y_rand_test, y_rand_pred, y_chaos_test, y_chaos_pred)
+    return 2 * np.sin(x) + 0.1 * x**2 - 2
 
 
 # %% [markdown]
 # # Ensembles, Random Forests, Gradient Boosted Trees
 
 # %% [markdown]
-# ## Ensemble Methods
 #
-# Idea: combine several estimators to improve their overal performance.
+# ## Ensemble Methoden
 #
-# - Averaging methods:
-#   - Independent estimators, average predictions
-#   - Reduces variance (overfitting)
-#   - Bagging, random forests
-# - Boosting methods:
-#   - Train estimators sequentially
-#   - Each estimator is trained to reduce the bias of its (combined) predecessors
+# Idee: Kombiniere mehrere Schätzungen, um die Gesamtleistung zu verbessern.
+#
+# - Averaging-Methoden:
+#   - Unabhängige Schätzer (Estimators), durchschnittliche Vorhersagen
+#   - Reduziert die Varianz (overfitting)
+#   - Bagging, Random Forests
+# - Boosting methoden:
+#   - Schätzer sequenziell trainieren
+#   – Jeder Schätzer wird darauf trainiert, die Verzerrung seiner (kombinierten)
+#     Vorgänger zu reduzieren
 
 # %% [markdown]
 # ### Bagging
 #
-# - Averaging method: build several estimators of the same type, average their results
-# - Needs some way to introduce differences between estimators
-#   - Otherwise variance is not reduced
-#   - Train on random subsets of the training data
-# - Reduce overfitting
-# - Work best with strong estimators (e.g., decision trees with (moderately) large depth)
+# - Durchschnitts-Methode: Erstelle mehrere Schätzer des gleichen Typs, bilde den
+#   Durchschnitt ihrer Ergebnisse
+# - Benötigt eine Möglichkeit, Unterschiede zwischen Schätzern einzuführen
+#   - Andernfalls wird die Varianz nicht reduziert
+#   - Trainiere mit zufälligen Teilmengen der Trainingsdaten
+# - Reduziert Overfitting
+# - Arbeitet am besten mit starken Schätzern (z.B. Entscheidungsbäumen mit (mäßig)
+#   großer Tiefe)
 
 # %% [markdown]
 # ### Random Forests
 #
-# - Bagging classifier/regressor using decision trees
-# - For each tree in the forest:
-#   - Subset of training data
-#   - Subset of features
-# - Often significant reduction in variance (overfitting)
-# - Sometimes increase in bias
+# - Bagging classifier/regressor, der Entscheidungsbäume verwendet
+# - Für jeden Baum im Wald:
+#   - Teilmenge von Trainingsdaten
+#   - Teilmenge von Merkmalen
+# - Oft signifikante Verringerung der Varianz (Overfitting)
+# - Manchmal Erhöhung des Bias
 
 # %%
 from sklearn.ensemble import RandomForestRegressor
 
 # %%
-evaluate_non_random_regressor(RandomForestRegressor, lin, random_state=42);
-
-# %%
-evaluate_non_random_regressor(RandomForestRegressor, fun, random_state=42);
-
-# %%
-evaluate_non_random_regressor(
-    RandomForestRegressor, fun, n_estimators=25, criterion="absolute_error", random_state=42
-);
-
-# %%
-evaluate_regressor(RandomForestRegressor, lin, random_state=42);
-
-# %%
-evaluate_regressor(
-    RandomForestRegressor, lin, n_estimators=500, max_depth=3, random_state=42
+et.evaluate_non_random_regressor(
+    RandomForestRegressor, lin, x_train, x_test, random_state=42
 )
 
 # %%
-evaluate_regressor(
-    RandomForestRegressor, lin, n_estimators=500, min_samples_leaf=6, random_state=42
+et.evaluate_non_random_regressor(
+    RandomForestRegressor, fun, x_train, x_test, random_state=42
 )
 
 # %%
-evaluate_regressor(RandomForestRegressor, fun, random_state=42)
-
-# %%
-evaluate_regressor(
+et.evaluate_non_random_regressor(
     RandomForestRegressor,
     fun,
+    x_train,
+    x_test,
+    n_estimators=25,
+    criterion="absolute_error",
+    random_state=42,
+)
+
+# %%
+et.evaluate_regressor(RandomForestRegressor, lin, x_train, x_test, random_state=42)
+
+# %%
+et.evaluate_regressor(
+    RandomForestRegressor,
+    lin,
+    x_train,
+    x_test,
+    n_estimators=500,
+    max_depth=3,
+    random_state=42,
+)
+
+# %%
+et.evaluate_regressor(
+    RandomForestRegressor,
+    lin,
+    x_train,
+    x_test,
+    n_estimators=500,
+    min_samples_leaf=6,
+    random_state=42,
+)
+
+# %%
+et.evaluate_regressor(RandomForestRegressor, fun, x_train, x_test, random_state=42)
+
+# %%
+et.evaluate_regressor(
+    RandomForestRegressor,
+    fun,
+    x_train,
+    x_test,
     n_estimators=1000,
     min_samples_leaf=6,
     random_state=43,
@@ -213,51 +147,83 @@ evaluate_regressor(
 # %% [markdown]
 # ## Gradient Boosted Trees
 #
-# - Boosting method for both regression and classification
-# - Requires differentiable loss function
+# - Boosting-Methode sowohl für die Regression als auch für die Klassifizierung
+# - Benötigt differenzierbare Verlustfunktion
 
 # %%
 from sklearn.ensemble import GradientBoostingRegressor
 
 # %%
-evaluate_non_random_regressor(GradientBoostingRegressor, lin);
+et.evaluate_non_random_regressor(GradientBoostingRegressor, lin, x_train, x_test)
 
 # %%
-evaluate_non_random_regressor(GradientBoostingRegressor, fun);
+et.evaluate_non_random_regressor(GradientBoostingRegressor, fun, x_train, x_test)
 
 # %%
-evaluate_regressor(GradientBoostingRegressor, lin);
+et.evaluate_regressor(GradientBoostingRegressor, lin, x_train, x_test)
 
 # %%
-evaluate_regressor(GradientBoostingRegressor, lin, n_estimators=200, learning_rate=0.05, loss="absolute_error");
+et.evaluate_regressor(
+    GradientBoostingRegressor,
+    lin,
+    x_train,
+    x_test,
+    n_estimators=200,
+    learning_rate=0.05,
+    loss="absolute_error",
+)
 
 # %%
-evaluate_regressor(GradientBoostingRegressor, lin, n_estimators=500, learning_rate=0.01,
-                   loss="absolute_error", subsample=0.1, random_state=46);
+et.evaluate_regressor(
+    GradientBoostingRegressor,
+    lin,
+    x_train,
+    x_test,
+    n_estimators=500,
+    learning_rate=0.01,
+    loss="absolute_error",
+    subsample=0.1,
+    random_state=46,
+)
 
 # %%
-evaluate_regressor(GradientBoostingRegressor, fun, n_estimators=500, learning_rate=0.01,
-                   loss="absolute_error", subsample=0.1, random_state=44);
+et.evaluate_regressor(
+    GradientBoostingRegressor,
+    fun,
+    x_train,
+    x_test,
+    n_estimators=500,
+    learning_rate=0.01,
+    loss="absolute_error",
+    subsample=0.1,
+    random_state=44,
+)
 
 # %% [markdown]
-# ### Multiple Features
+# ## Mehrere Features
 
 # %%
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
+
 np.set_printoptions(precision=1)
 
 # %%
-x, y, coef = make_regression(n_samples=250, n_features=4, n_informative=1, coef=True, random_state=42)
+x, y, coef = make_regression(
+    n_samples=250, n_features=4, n_informative=1, coef=True, random_state=42
+)
 x.shape, y.shape, coef
 
 # %%
 fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(20, 12))
 for i, ax in enumerate(axs.reshape(-1)):
     sns.scatterplot(x=x[:, i], y=y, ax=ax)
+plt.show()
 
 # %%
-x, y, coef = make_regression(n_samples=250, n_features=20, n_informative=10, coef=True, random_state=42)
+x, y, coef = make_regression(
+    n_samples=250, n_features=20, n_informative=10, coef=True, random_state=42
+)
 x.shape, y.shape, coef
 
 # %%
@@ -267,9 +233,10 @@ x_train.shape, x_test.shape, y_train.shape, y_test.shape
 # %%
 fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(20, 12))
 for i in range(2):
-    sns.scatterplot(x=x[:, i], y=y, ax=axs[0, i]);
+    sns.scatterplot(x=x[:, i], y=y, ax=axs[0, i])
 for i in range(2):
-    sns.scatterplot(x=x[:, i + 6], y=y, ax=axs[1, i]);
+    sns.scatterplot(x=x[:, i + 6], y=y, ax=axs[1, i])
+plt.show()
 
 # %%
 lr_clf = LinearRegression()
@@ -303,7 +270,14 @@ y_gb_pred = gb_clf.predict(x_test)
 mean_absolute_error(y_test, y_gb_pred), mean_squared_error(y_test, y_gb_pred)
 
 # %%
-x, y, coef = make_regression(n_samples=250, n_features=20, n_informative=10, noise=100.0, coef=True, random_state=42)
+x, y, coef = make_regression(
+    n_samples=250,
+    n_features=20,
+    n_informative=10,
+    noise=100.0,
+    coef=True,
+    random_state=42,
+)
 x.shape, y.shape, coef
 
 # %%
@@ -339,8 +313,14 @@ y_gb_pred = gb_clf.predict(x_test)
 mean_absolute_error(y_test, y_gb_pred), mean_squared_error(y_test, y_gb_pred)
 
 # %%
-x, y, coef = make_regression(n_samples=250, n_features=20, n_informative=10, noise=100.0,
-                             coef=True, random_state=42)
+x, y, coef = make_regression(
+    n_samples=250,
+    n_features=20,
+    n_informative=10,
+    noise=100.0,
+    coef=True,
+    random_state=42,
+)
 y += (20 * x[:, 1]) ** 2
 x.shape, y.shape, coef
 
@@ -351,9 +331,10 @@ x_train.shape, x_test.shape, y_train.shape, y_test.shape
 # %%
 fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(20, 12))
 for i in range(2):
-    sns.scatterplot(x=x[:, i], y=y, ax=axs[0, i]);
+    sns.scatterplot(x=x[:, i], y=y, ax=axs[0, i])
 for i in range(2):
-    sns.scatterplot(x=x[:, i + 6], y=y, ax=axs[1, i]);
+    sns.scatterplot(x=x[:, i + 6], y=y, ax=axs[1, i])
+plt.show()
 
 # %%
 lr_clf = LinearRegression()
@@ -382,120 +363,3 @@ gb_clf.fit(x_train, y_train)
 y_gb_pred = gb_clf.predict(x_test)
 
 mean_absolute_error(y_test, y_gb_pred), mean_squared_error(y_test, y_gb_pred)
-
-# %% [markdown]
-#
-#  ## Feature Engineering
-
-# %%
-x = rng.uniform(size=(150, 1), low=0.0, high=10.0)
-x_train, x_test = x[:100], x[100:]
-x_plot = np.linspace(0, 10, 500)
-x_train[:3]
-
-# %%
-y_lin_train = lin(x_train).reshape(-1)
-y_lin_test = lin(x_test).reshape(-1)
-y_fun_train = fun(x_train.reshape(-1))
-y_fun_test = fun(x_test).reshape(-1)
-
-# %%
-x_squares = x * x
-x_squares[:3]
-
-# %%
-x_sins = np.sin(x)
-x_sins[:3]
-
-# %%
-x_train_aug = np.concatenate([x_train, x_train * x_train, np.sin(x_train)], axis=1)
-x_train_aug[:3]
-
-# %%
-x_test_aug = np.concatenate([x_test, x_test * x_test, np.sin(x_test)], axis=1)
-
-# %%
-# from sklearn.linear_model import Ridge
-# lr_aug_lin = Ridge()
-lr_aug_lin = LinearRegression()
-lr_aug_lin.fit(x_train_aug, y_lin_train);
-
-# %%
-lr_aug_lin.coef_, lr_aug_lin.intercept_
-
-
-# %%
-y_aug_lin_pred = lr_aug_lin.predict(x_test_aug)
-
-# %%
-mean_absolute_error(y_lin_test, y_aug_lin_pred), mean_squared_error(
-    y_lin_test, y_aug_lin_pred
-)
-
-# %%
-x_test.shape, x_plot.shape
-
-
-# %%
-def train_and_plot_aug(f_y, scale=0.5):
-    y_plot = f_y(x_plot)
-
-    f_r = lambda x: randomize(f_y, x, scale=scale)
-    y_train = f_r(x_train_aug[:, 0])
-    y_test = f_r(x_test)
-
-    lr_aug = LinearRegression() # Try with Ridge() as well...
-    lr_aug.fit(x_train_aug, y_train)
-    y_pred_test = lr_aug.predict(
-                      np.concatenate([x_test, x_test * x_test, np.sin(x_test)], axis=1)
-                   )
-    x_plot2 = x_plot.reshape(-1, 1)
-    y_pred_plot = lr_aug.predict(
-                     np.concatenate([x_plot2, x_plot2 * x_plot2, np.sin(x_plot2)], axis=1)
-                  )
-
-    fig, ax = plt.subplots(figsize=(12, 6))
-    sns.scatterplot(x=x_plot2[:, 0], y=y_plot, color="orange")
-    sns.scatterplot(x=x_plot2[:, 0], y=y_pred_plot, color="red")
-    sns.scatterplot(x=x_train_aug[:, 0], y=y_train, color="green")
-    plt.show()
-
-    mae_in = mean_absolute_error(y_test, y_pred_test)
-    mse_in = mean_absolute_error(y_test, y_pred_test)
-    rmse_in = np.sqrt(mse_in)
-
-    y_nr = f_y(x_test)
-    mae_true = mean_absolute_error(y_nr, y_pred_test)
-    mse_true = mean_absolute_error(y_nr, y_pred_test)
-    rmse_true = np.sqrt(mse_true)
-
-    print(f"Vs. input: MAE: {mae_in:.2f}, MSE: {mse_in:.2f}, RMSE: {rmse_in:.2f}")
-    print(f"True:      MAE: {mae_true:.2f}, MSE: {mse_true:.2f}, RMSE: {rmse_true:.2f}")
-    print(f"Parameters: {lr_aug.coef_}, {lr_aug.intercept_}")
-
-
-# %%
-train_and_plot_aug(lin)
-
-# %%
-train_and_plot_aug(fun, scale=0.0)
-
-# %%
-train_and_plot_aug(fun, scale=0.5)
-
-# %%
-train_and_plot_aug(fun, scale=1.5)
-
-# %%
-train_and_plot_aug(fun, scale=3)
-
-
-# %%
-def fun2(x): return 2.8 * np.sin(x) + 0.3 * x + 0.08 * x ** 2 - 2.5
-
-train_and_plot_aug(fun2, scale=1.5)
-
-# %%
-train_and_plot_aug(lambda x: np.select([x<=6, x>6], [-0.5, 3.5]))
-
-# %%
---
+++ Folien/Python/Completed/Woche 4/03 Feature-Engineering.py
@@ -0,0 +1,183 @@
+# %% [markdown]
+#
+# <div style="text-align:center; font-size:200%;">
+#  <b>Feature-Engineering</b>
+# </div>
+# <br/>
+# <div style="text-align:center;">Dr. Matthias Hölzl</div>
+# <br/>
+
+# %% [markdown]
+#
+# # Feature-Engineering
+
+# %%
+import numpy as np
+import seaborn as sns
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.linear_model import LinearRegression
+import evaluation_tools as et
+
+# %%
+sns.set_theme()
+
+# %%
+rng = np.random.default_rng(42)
+
+
+# %%
+def lin(x):
+    return 0.85 * x - 1.5
+
+
+# %%
+def fun(x):
+    return 0.1 * x**2 - 2 + 2 * np.sin(x)
+
+
+# %% [markdown]
+#
+# ### Trainieren eines linearen Modells
+
+# %%
+x = rng.uniform(size=(150, 1), low=0.0, high=10.0)
+x_train, x_test = x[:100], x[100:]
+x_plot = np.linspace(0, 10, 500)
+x_train[:3]
+
+# %%
+y_lin_train = lin(x_train).reshape(-1)
+y_lin_test = lin(x_test).reshape(-1)
+y_fun_train = fun(x_train.reshape(-1))
+y_fun_test = fun(x_test).reshape(-1)
+
+# %% [markdown]
+#
+# Linearer Regressor für lineare Funktion
+
+# %%
+lr_lin = LinearRegression()
+lr_lin.fit(x_train, y_lin_train)
+
+# %%
+lr_lin.coef_, lr_lin.intercept_
+
+# %%
+et.evaluate_regressor(LinearRegression, lin, x_train, x_test)
+
+# %% [markdown]
+#
+# Linearer Regressor für nicht-lineare Funktion
+
+# %%
+et.evaluate_regressor(LinearRegression, fun, x_train, x_test)
+
+# %% [markdown]
+#
+# ### Feature-Augmentation
+#
+# - Idee: Wir geben dem linearen Regressor mehr Features, die nicht-linear sind.
+# - Idealerweise die Funktionen von `x`, die in der Funktion `f_y` vorkommen.
+# - $\mathit{lin}(x) = 0.85 \cdot x - 1.5$
+# - $\mathit{fun}(x) = 0.1 \cdot x^2 - 2 + 2 \cdot \sin(x)$
+
+# %%
+x_train_aug = np.concatenate([x_train, x_train * x_train, np.sin(x_train)], axis=1)
+x_train_aug[:3]
+
+# %% [markdown]
+#
+# - Trainieren eines linearen Regressors mit augmentierten Features
+# - Lineare Funktion (mit Rauschen) als Zielvariable
+
+
+# %%
+x_test_aug = np.concatenate([x_test, x_test * x_test, np.sin(x_test)], axis=1)
+
+# %%
+lr_aug_lin = LinearRegression()
+lr_aug_lin.fit(x_train_aug, y_lin_train)
+
+# %% [markdown]
+# $\mathit{lin}(x) = 0.85 \cdot x - 1.5$
+
+# %%
+lr_aug_lin.coef_, lr_aug_lin.intercept_
+
+# %%
+lin_reg, lin_reg_rand, lin_reg_chaos = et.evaluate_regressor(
+    LinearRegression, lin, x_train_aug, x_test_aug, use_x_train_for_plot=True
+)
+
+# %% [markdown]
+# $\mathit{lin}(x) = 0.85 \cdot x - 1.5$
+
+# %%
+lin_reg.coef_, lin_reg.intercept_
+
+# %%
+lin_reg_rand.coef_, lin_reg_rand.intercept_
+
+# %%
+lin_reg_chaos.coef_, lin_reg_chaos.intercept_
+
+# %% [markdown]
+#
+# - Trainieren eines linearen Regressors mit augmentierten Features
+# - Nicht-lineare Funktion (mit Rauschen) als Zielvariable
+
+# %%
+fun_reg, fun_reg_rand, fun_reg_chaos = et.evaluate_regressor(
+    LinearRegression, fun, x_train_aug, x_test_aug, use_x_train_for_plot=True
+)
+
+# %% [markdown]
+# $\mathit{fun}(x) = 0.1 \cdot x^2 - 2 + 2 \cdot \sin(x)$
+
+# %%
+fun_reg.coef_, fun_reg.intercept_
+
+# %%
+fun_reg_rand.coef_, fun_reg_rand.intercept_
+
+# %%
+fun_reg_chaos.coef_, fun_reg_chaos.intercept_
+
+# %%
+et.train_and_plot_aug(lin, x_train, x_plot, x_test)
+
+# %%
+et.train_and_plot_aug(fun, x_train, x_plot, x_test, scale=0.0)
+
+# %%
+et.train_and_plot_aug(fun, x_train, x_plot, x_test, scale=0.5)
+
+# %%
+et.train_and_plot_aug(fun, x_train, x_plot, x_test, scale=1.5)
+
+# %%
+et.train_and_plot_aug(fun, x_train, x_plot, x_test, scale=3)
+
+
+# %%
+def fun2(x):
+    return 2.8 * np.sin(x) + 0.3 * x + 0.08 * x**2 - 2.5
+
+
+# %%
+et.train_and_plot_aug(fun2, x_train, x_plot, x_test, scale=1.5)
+
+# %%
+def fun3(x):
+    return np.sin(x) + 0.3 * x + 0.01 * x**3 - 2.5
+
+# %%
+et.train_and_plot_aug(fun3, x_train, x_plot, x_test, scale=0.5)
+
+
+# %%
+et.train_and_plot_aug(
+    lambda x: np.select([x <= 6, x > 6], [-0.5, 3.5]), x_train, x_plot, x_test
+)
+
+# %%
